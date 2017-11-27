15b38de47e9a5269ac4768c378847aee60f546b0
Ftlka <smart.Julia@yandex.ru>
2017-11-27 15:30:12 +0500

    Initial commit

--- async.js
+++ async.js
@@ -3,11 +3,37 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
-/** Функция паралелльно запускает указанное число промисов
- * @param {Array} jobs – функции, которые возвращают промисы
- * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
- */
+const makeArrOfPromises = (jobs, timeout) => jobs.map(job => () =>
+    new Promise((resolve, reject) => {
+        job().then(resolve, reject);
+        setTimeout(() => reject(new Error('timed out')), timeout);
+    }));
+
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise((resolve) => {
+        if (!jobs.length) {
+            resolve([]);
+        }
+        const result = [];
+        let finishedJobs = 0;
+        let currentJob = 0;
+        const promises = makeArrOfPromises(jobs, timeout);
+        promises.slice(0, parallelNum)
+            .forEach((promise, idx) => start(promise, idx));
+
+        function start(promise) {
+            const idx = currentJob++;
+            const handler = data => finish(data, idx);
+            promise().then(handler);
+        }
+
+        function finish(data, idx) {
+            result[idx] = data;
+            finishedJobs++;
+            if (jobs.length === finishedJobs) {
+                resolve(result);
+            }
+            start(promises[currentJob]);
+        }
+    });
 }
